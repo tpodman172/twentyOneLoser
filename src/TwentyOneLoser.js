351dae4573012c4f3db2271c01674cda4f454311
takaharu-nakase <takaharu.nakase@bizreach.co.jp>
2023-08-24 15:30:32 +0900

    プレイヤーが負けるとコンピュータの負けと表示される不具合を修正

--- src/TwentyOneLoser.js
+++ src/TwentyOneLoser.js
@@ -17,7 +17,7 @@ const Game = () => {
 
     const computerPlay = () => {
         let value = Math.min(randomNumber(), 21 - count); // Always choose a value that will not make the count exceed 21.
-        if (turn === "computer") {
+        if (turn === "computer" && count !== 21) {
             setTimeout(() => {
                 handleClick(value);
             }, 1000);
@@ -25,11 +25,11 @@ const Game = () => {
     };
 
     computerPlay(); // trigger computer play when it's computer's turn
-
+    // 三項演算子
     return (
         <div className="game">
             <h1>いまのすうじ : {count}</h1>
-            <p>It's {turn}'s turn</p>
+            <p> {turn === 'computer' ? 'コンピュータ' : 'プレイヤー'}の番 </p>
             {[1, 2, 3].map((value) => (
                 <button
                     key={value}
@@ -39,7 +39,7 @@ const Game = () => {
                     + {value}
                 </button>
             ))}
-            {count >= 21 && <p>{turn === "player" ? "Computer" : "Player"} loses</p>}
+            {count >= 21 && <p>{turn === "player" ? "コンピュータ" : "プレイヤー"} の負け</p>}
             <div>
                 <button
                     onClick={() => handleRetry()}
